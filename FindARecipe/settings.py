2903435242ff45d6774e9e78d87d0ac2ec19947b
kenny101 <53395124+kenny101@users.noreply.github.com>
2021-01-04 01:23:24 -0800

    Update settings.py

--- FindARecipe/settings.py
+++ FindARecipe/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zbpvuo$&92+(21&n#5-msw*0klh&777a)32tjq2&nv9c47(z!1'
+SECRET_KEY = 'Secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
